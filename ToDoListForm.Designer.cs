a1691a4aa994932b8cac48ef72d2502c7ceb9a1f
Ivan <ermilevivanggu@gmail.com>
2024-07-26 19:32:03 +0300

    I made a ToDoList

---
+++ ToDoListForm.Designer.cs
@@ -0,0 +1,176 @@
+﻿namespace Coldi
+{
+    partial class ToDoListForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            tasksListBox = new ListBox();
+            addBTN = new Button();
+            WriteTaskTextBox = new TextBox();
+            DeleteBTN = new Button();
+            СhangeBTN = new Button();
+            label1 = new Label();
+            ChangeBox = new TextBox();
+            CloseBTN = new Button();
+            executionListBox = new ListBox();
+            executionBTN = new Button();
+            SuspendLayout();
+            // 
+            // tasksListBox
+            // 
+            tasksListBox.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point, 204);
+            tasksListBox.FormattingEnabled = true;
+            tasksListBox.ItemHeight = 15;
+            tasksListBox.Location = new Point(12, 121);
+            tasksListBox.Name = "tasksListBox";
+            tasksListBox.Size = new Size(365, 319);
+            tasksListBox.TabIndex = 0;
+            // 
+            // addBTN
+            // 
+            addBTN.Location = new Point(12, 92);
+            addBTN.Name = "addBTN";
+            addBTN.Size = new Size(75, 23);
+            addBTN.TabIndex = 1;
+            addBTN.Text = "Add";
+            addBTN.UseVisualStyleBackColor = true;
+            addBTN.Click += addBTN_Click;
+            // 
+            // WriteTaskTextBox
+            // 
+            WriteTaskTextBox.Location = new Point(113, 27);
+            WriteTaskTextBox.Name = "WriteTaskTextBox";
+            WriteTaskTextBox.Size = new Size(100, 23);
+            WriteTaskTextBox.TabIndex = 2;
+            // 
+            // DeleteBTN
+            // 
+            DeleteBTN.Location = new Point(93, 92);
+            DeleteBTN.Name = "DeleteBTN";
+            DeleteBTN.Size = new Size(75, 23);
+            DeleteBTN.TabIndex = 3;
+            DeleteBTN.Text = " Delete";
+            DeleteBTN.UseVisualStyleBackColor = true;
+            DeleteBTN.Click += DeleteBTN_Click;
+            // 
+            // СhangeBTN
+            // 
+            СhangeBTN.Location = new Point(174, 92);
+            СhangeBTN.Name = "СhangeBTN";
+            СhangeBTN.Size = new Size(75, 23);
+            СhangeBTN.TabIndex = 0;
+            СhangeBTN.Text = "To change";
+            СhangeBTN.UseVisualStyleBackColor = true;
+            СhangeBTN.Click += СhangeBTN_Click;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(3, 27);
+            label1.Name = "label1";
+            label1.Size = new Size(108, 15);
+            label1.TabIndex = 5;
+            label1.Text = "Напишите задачу";
+            // 
+            // ChangeBox
+            // 
+            ChangeBox.BorderStyle = BorderStyle.FixedSingle;
+            ChangeBox.Location = new Point(163, 148);
+            ChangeBox.Name = "ChangeBox";
+            ChangeBox.Size = new Size(100, 23);
+            ChangeBox.TabIndex = 6;
+            ChangeBox.Visible = false;
+            // 
+            // CloseBTN
+            // 
+            CloseBTN.BackColor = Color.Red;
+            CloseBTN.FlatStyle = FlatStyle.Popup;
+            CloseBTN.Font = new Font("Segoe UI", 15.75F, FontStyle.Bold, GraphicsUnit.Point, 204);
+            CloseBTN.Location = new Point(461, 12);
+            CloseBTN.Name = "CloseBTN";
+            CloseBTN.Size = new Size(47, 38);
+            CloseBTN.TabIndex = 7;
+            CloseBTN.Text = "X";
+            CloseBTN.UseVisualStyleBackColor = false;
+            CloseBTN.Click += CloseBTN_Click;
+            // 
+            // executionListBox
+            // 
+            executionListBox.FormattingEnabled = true;
+            executionListBox.ItemHeight = 15;
+            executionListBox.Location = new Point(257, 121);
+            executionListBox.Name = "executionListBox";
+            executionListBox.Size = new Size(120, 319);
+            executionListBox.TabIndex = 8;
+            // 
+            // executionBTN
+            // 
+            executionBTN.Location = new Point(383, 121);
+            executionBTN.Name = "executionBTN";
+            executionBTN.Size = new Size(75, 23);
+            executionBTN.TabIndex = 9;
+            executionBTN.Text = "Notice";
+            executionBTN.UseVisualStyleBackColor = true;
+            executionBTN.Click += executionBTN_Click;
+            // 
+            // ToDoListForm
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(520, 479);
+            Controls.Add(executionBTN);
+            Controls.Add(ChangeBox);
+            Controls.Add(executionListBox);
+            Controls.Add(CloseBTN);
+            Controls.Add(СhangeBTN);
+            Controls.Add(label1);
+            Controls.Add(DeleteBTN);
+            Controls.Add(WriteTaskTextBox);
+            Controls.Add(addBTN);
+            Controls.Add(tasksListBox);
+            FormBorderStyle = FormBorderStyle.None;
+            Name = "ToDoListForm";
+            Text = "ToDoListForm";
+            Load += ToDoListForm_Load;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private ListBox tasksListBox;
+        private Button addBTN;
+        private TextBox WriteTaskTextBox;
+        private Button DeleteBTN;
+        private Label label1;
+        private Button СhangeBTN;
+        private TextBox ChangeBox;
+        private Button CloseBTN;
+        private ListBox executionListBox;
+        private Button executionBTN;
+    }
+}
---
+++ ToDoListForm.cs
@@ -0,0 +1,243 @@
+﻿using Npgsql;
+using NpgsqlTypes;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Coldi
+{
+    public partial class ToDoListForm : Form
+    {
+        public ToDoListForm()
+        {
+            InitializeComponent();
+        }
+
+
+        public string task;
+        public string SelectTask;
+
+        string vStrConnection = "Server= localhost; port= 5432; user id= postgres; password= 93538565; database= todolistdb;";
+
+        NpgsqlCommand vCmd;
+        NpgsqlConnection vCon;
+
+
+
+        private void addBTN_Click(object sender, EventArgs e)
+        {
+            task = WriteTaskTextBox.Text;
+            try
+            {
+                connection();
+                using (vCmd = new NpgsqlCommand("INSERT INTO tasktodo(task) VALUES (@task)", vCon))
+                {
+                    vCmd.Parameters.Add("@task", NpgsqlDbType.Text).Value = task;
+
+                    vCmd.ExecuteNonQuery(); // Execute the INSERT query
+
+                    vCon.Close();
+
+                }
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+            executionListBox.Items.Clear();
+            EXElistLoad();
+            tasksListBox.Items.Clear();
+            listLoad();
+
+        }
+
+        private void ToDoListForm_Load(object sender, EventArgs e)
+        {
+            listLoad();
+            EXElistLoad();
+
+        }
+
+        private void DeleteBTN_Click(object sender, EventArgs e)
+        {
+
+            task = tasksListBox.SelectedItem.ToString();
+            try
+            {
+                connection();
+                using (vCmd = new NpgsqlCommand("DELETE FROM tasktodo WHERE task = @task", vCon))
+                {
+                    vCmd.Parameters.Add("@task", NpgsqlDbType.Text).Value = task;
+
+                    vCmd.ExecuteNonQuery(); // Execute the INSERT query
+
+                    vCon.Close();
+
+                }
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+            executionListBox.Items.Clear();
+            EXElistLoad();
+            tasksListBox.Items.Clear();
+            listLoad();
+        }
+
+        private void СhangeBTN_Click(object sender, EventArgs e)
+        {
+            ChangeBox.Visible = true;
+
+
+
+            if (ChangeBox.Text != "")
+            {
+                task = ChangeBox.Text;
+                SelectTask = tasksListBox.SelectedItem.ToString();
+                try
+                {
+                    connection();
+                    using (vCmd = new NpgsqlCommand("Update tasktodo set task = @task Where task = @seltask", vCon))
+                    {
+                        vCmd.Parameters.Add("@task", NpgsqlDbType.Text).Value = task;
+                        vCmd.Parameters.Add("@seltask", NpgsqlDbType.Text).Value = SelectTask;
+
+                        vCmd.ExecuteNonQuery(); // Execute the INSERT query
+
+                        vCon.Close();
+
+                    }
+
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+
+                executionListBox.Items.Clear();
+                EXElistLoad();
+                tasksListBox.Items.Clear();
+                listLoad();
+            }
+        }
+
+
+
+        private void CloseBTN_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+
+        private void listLoad()
+        {
+
+            try
+            {
+                connection();
+                using (vCmd = new NpgsqlCommand("SELECT task FROM tasktodo", vCon))
+                {
+                    using (NpgsqlDataReader reader = vCmd.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+
+                            tasksListBox.Items.Add(Text = reader["task"].ToString());
+
+                        }
+                    }
+                }
+                vCon.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+      
+        private void executionBTN_Click(object sender, EventArgs e)
+        {
+            executionCheck();
+        }
+
+        private void executionCheck()
+        {
+            
+            SelectTask = tasksListBox.SelectedItem.ToString();
+
+            try
+            {
+                connection();
+                using (vCmd = new NpgsqlCommand("Update tasktodo set executiontask = 'Выполнено' Where task = @seltask", vCon))
+                {
+                    vCmd.Parameters.Add("@seltask", NpgsqlDbType.Text).Value = SelectTask;
+                   
+
+                    vCmd.ExecuteNonQuery(); // Execute the INSERT query
+
+                    vCon.Close();
+                }
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+
+            tasksListBox.Items.Clear();
+            executionListBox.Items.Clear();
+            EXElistLoad();
+            listLoad();
+        }
+
+        private void EXElistLoad()
+        {
+
+            try
+            {
+                connection();
+                using (vCmd = new NpgsqlCommand("SELECT executiontask FROM tasktodo", vCon))
+                {
+                    using (NpgsqlDataReader reader = vCmd.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+
+                            executionListBox.Items.Add(Text = reader["executiontask"].ToString());
+
+                        }
+                    }
+                }
+                vCon.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void connection()
+        {
+            vCon = new NpgsqlConnection();
+            vCon.ConnectionString = vStrConnection;//connect 
+
+            if (vCon.State == ConnectionState.Closed)
+            {
+                vCon.Open();
+            }
+        }
+    }
+}
